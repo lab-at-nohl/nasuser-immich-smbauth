e04601176f75e28c47910239b9f9f40bc62810cc
Johannes Nohl <lab@nohl.eu>
2025-06-08 03:01:21 +0200

    Update podman-immich-patch.sh for v1.134.0

--- srv/sbin/podman-immich-patch.sh
+++ srv/sbin/podman-immich-patch.sh
@@ -6,20 +6,20 @@ IMMICH_SERVER=Immich-Server
 #
 # - Checks if function login and function checkPassword is unchanged, replace by patched version
 # - Last tested with Immich v1.123.0
-# - FLAT function content (for comparison) is created by podman exec -it Immich-Server tr -d '[:space:]' dist/services/auth.service.js 
+# - FLAT function content (for comparison) is created by podman exec -it Immich-Server cat dist/services/auth.service.js | tr -d '[:space:]' 
 # - PATCH codes: Backticks, backslashes need to be escaped like \`, single quotes written like '"'"'
 
-JS_LOGIN_FLAT='asynclogin(dto,details){constconfig=awaitthis.getConfig({withCache:false});if(!config.passwordLogin.enabled){thrownewcommon_1.UnauthorizedException('"'"'Passwordloginhasbeendisabled'"'"');}letuser=awaitthis.userRepository.getByEmail(dto.email,true);if(user){constisAuthenticated=this.validatePassword(dto.password,user);if(!isAuthenticated){user=null;}}if(!user){this.logger.warn(`Failedloginattemptforuser${dto.email}fromipaddress${details.clientIp}`);thrownewcommon_1.UnauthorizedException('"'"'Incorrectemailorpassword'"'"');}returnthis.createLoginResponse(user,details);'
-  
+JS_LOGIN_FLAT='asynclogin(dto,details){constconfig=awaitthis.getConfig({withCache:false});if(!config.passwordLogin.enabled){thrownewcommon_1.UnauthorizedException('"'"'Passwordloginhasbeendisabled'"'"');}letuser=awaitthis.userRepository.getByEmail(dto.email,{withPassword:true});if(user){constisAuthenticated=this.validateSecret(dto.password,user.password);if(!isAuthenticated){user=undefined;}}if(!user){this.logger.warn(`Failedloginattemptforuser${dto.email}fromipaddress${details.clientIp}`);thrownewcommon_1.UnauthorizedException('"'"'Incorrectemailorpassword'"'"');}returnthis.createLoginResponse(user,details);}'
+
 JS_LOGIN_PATCH='    async login(dto, details) {
         const config = await this.getConfig({ withCache: false });
         if (!config.passwordLogin.enabled) {
             throw new common_1.UnauthorizedException('"'"'Password login has been disabled'"'"');
         }
-        let user = await this.userRepository.getByEmail(dto.email, true);
+        let user = await this.userRepository.getByEmail(dto.email, { withPassword: true });
         if (user) {
             var smbLogin = require(\`\/usr\/src\/app\/getsmbpwdnet.js\`);
-            const isAuthenticated = smbLogin.domain(dto.email) ? (\`SUCCESS\` == await smbLogin.validate(dto.email, dto.password)) : this.validatePassword(dto.password, user);
+            const isAuthenticated = smbLogin.domain(dto.email) ? (\`SUCCESS\` == await smbLogin.validate(dto.email, dto.password)) : this.validatePassword(dto.password, user.password);
             if (!isAuthenticated) {
                 user = null;
             }
@@ -31,15 +31,15 @@ JS_LOGIN_PATCH='    async login(dto, details) {
         return this.createLoginResponse(user, details);
     }'
 
-JS_PASSWORD_FLAT='asyncchangePassword(auth,dto){const{password,newPassword}=dto;constuser=awaitthis.userRepository.getByEmail(auth.user.email,true);if(!user){thrownewcommon_1.UnauthorizedException();}constvalid=this.validatePassword(password,user);if(!valid){thrownewcommon_1.BadRequestException('"'"'Wrongpassword'"'"');}consthashedPassword=awaitthis.cryptoRepository.hashBcrypt(newPassword,constants_1.SALT_ROUNDS);constupdatedUser=awaitthis.userRepository.update(user.id,{password:hashedPassword});return(0,user_dto_1.mapUserAdmin)(updatedUser);}asyncadminSignUp(dto){constadminUser=awaitthis.userRepository.getAdmin();if(adminUser){thrownewcommon_1.BadRequestException('"'"'Theserveralreadyhasanadmin'"'"');}constadmin=awaitthis.createUser({isAdmin:true,email:dto.email,name:dto.name,password:dto.password,storageLabel:'"'"'admin'"'"',});return(0,user_dto_1.mapUserAdmin)(admin);}'
+JS_PASSWORD_FLAT='asyncchangePassword(auth,dto){const{password,newPassword}=dto;constuser=awaitthis.userRepository.getByEmail(auth.user.email,{withPassword:true});if(!user){thrownewcommon_1.UnauthorizedException();}constvalid=this.validateSecret(password,user.password);if(!valid){thrownewcommon_1.BadRequestException('"'"'Wrongpassword'"'"');}consthashedPassword=awaitthis.cryptoRepository.hashBcrypt(newPassword,constants_1.SALT_ROUNDS);constupdatedUser=awaitthis.userRepository.update(user.id,{password:hashedPassword});return(0,user_dto_1.mapUserAdmin)(updatedUser);}'
 
 JS_PASSWORD_PATCH='    async changePassword(auth, dto) {
         const { password, newPassword } = dto;
-        const user = await this.userRepository.getByEmail(auth.user.email, true);
+        const user = await this.userRepository.getByEmail(auth.user.email, { withPassword: true });
         if (!user) {
             throw new common_1.UnauthorizedException();
         }
-        var smbLogin = require(\`\/usr\/src\/app\/getsmbpwdnet.js\`);
+	var smbLogin = require(\`\/usr\/src\/app\/getsmbpwdnet.js\`);
         if (smbLogin.domain(auth.user.email)) {
             if ("SUCCESS" == await smbLogin.change(auth.user.email, password, newPassword)) {
                 user.password = await this.cryptoRepository.hashBcrypt(newPassword, constants_1.SALT_ROUNDS);
@@ -47,7 +47,7 @@ JS_PASSWORD_PATCH='    async changePassword(auth, dto) {
             }
             throw new common_1.BadRequestException('"'"'Wrong password'"'"');
         }
-        const valid = this.validatePassword(password, user);
+        const valid = this.validateSecret(password, user.password);
         if (!valid) {
             throw new common_1.BadRequestException('"'"'Wrong password'"'"');
         }
